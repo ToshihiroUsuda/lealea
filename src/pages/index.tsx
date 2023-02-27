0e4ddac0f1913cd8ed02ed88144eb86ba4deb691
Toshihiro Usuda <jizai.labo@gmail.com>
2023-02-27 21:13:00 +0900

    head title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material'
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
 
 import Layout from '../components/shell/layout'
@@ -7,13 +8,14 @@ import Layout from '../components/shell/layout'
 const style: React.CSSProperties = {
   objectFit: 'contain',
   objectPosition: 'top',
-  // maxWidth: '100%',
-  // height: 'auto',
 }
 
 const Home: NextPage = () => {
   return (
     <Layout home needInquiry>
+      <Head>
+        <title>KOA Consulting Farm</title>
+      </Head>
       <Box sx={{ pt: 8, pb: 16 }}>
         <Box sx={{ position: 'relative', height: 'min(141vw, 160vh)' }}>
           <Image src='/LP/lp1.jpg' alt='lp1' fill style={style} />
