c0237812429fd9585eb0ef37ead25f9efbcead98
Toshihiro Usuda <jizai.labo@gmail.com>
2023-02-28 20:44:00 +0900

    inquirybutton layout

--- src/components/inquiry/inquiryButton.tsx
+++ src/components/inquiry/inquiryButton.tsx
@@ -9,14 +9,25 @@ const InquiryButton = () => {
   return (
     <Box
       role='presentation'
-      sx={{ position: 'fixed', bottom: 64, left: '50%', transform: 'translateX(-50%)' }}
+      sx={{
+        position: 'fixed',
+        bottom: { xs: 32, sm: 48 },
+        left: '50%',
+        transform: 'translateX(-50%)',
+      }}
     >
       <Link href='/contact'>
         <Fab
           variant='extended'
           color='primary'
           aria-label='add'
-          sx={{ p: 6, borderRadius: 100, borderColor: 'white', borderWidth: 3, width: 240 }}
+          sx={{
+            p: { xs: 3, sm: 5 },
+            borderRadius: 100,
+            borderColor: 'white',
+            borderWidth: 3,
+            width: 240,
+          }}
         >
           <Typography variant='h5'>お問い合せ</Typography>
         </Fab>
