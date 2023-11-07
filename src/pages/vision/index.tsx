491db34edb020c2c9152d1703a4dd10f4e6f45d2
tshrusd <tshr.usd@gmail.com>
2023-11-07 21:02:16 +0900

    precident picture

--- src/pages/vision/index.tsx
+++ src/pages/vision/index.tsx
@@ -1,7 +1,9 @@
 import { Box, Typography } from '@mui/material'
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 
+import { Column } from '../../components/common/styledComponents'
 import Background from '../../components/shell/background'
 import Layout from '../../components/shell/layout'
 import { getTopMessage } from '../../libs/message'
@@ -32,21 +34,37 @@ const VisionPage: NextPage<Props> = ({ message }: { message: string }) => {
         <Box sx={{ position: 'relative' }}>
           {/* <Image src='/LP1.png' alt='企業理念' fill style={style} /> */}
           <Background>
-            <Typography fontSize={{ xs: 32, sm: 48 }} fontFamily={fontFamily}>
-              私たちの想い
-            </Typography>
-            <Box pt={6}>
-              {sentences.map((sentence, index) => (
-                <Typography
-                  key={index}
-                  fontFamily={fontFamily}
-                  fontSize={{ xs: 16, sm: 24 }}
-                  pt={sentence !== '' ? 1 : 2}
-                >
-                  {sentence}
-                </Typography>
-              ))}
-            </Box>
+            <Column alignItems='center'>
+              <Typography sx={{ fontSize: { xs: 32, sm: 48 }, fontFamily: fontFamily, pb: 4 }}>
+                私たちの想い
+              </Typography>
+              <Box
+                sx={{
+                  position: 'relative',
+                  width: { xs: 180, sm: 240 },
+                  height: { xs: 180, sm: 240 },
+                }}
+              >
+                <Image
+                  src='/assets/precident.png'
+                  alt='代表'
+                  fill
+                  style={{ objectFit: 'cover', borderRadius: 120 }}
+                />
+              </Box>
+              <Box pt={4}>
+                {sentences.map((sentence, index) => (
+                  <Typography
+                    key={index}
+                    fontFamily={fontFamily}
+                    fontSize={{ xs: 16, sm: 24 }}
+                    pt={sentence !== '' ? 1 : 2}
+                  >
+                    {sentence}
+                  </Typography>
+                ))}
+              </Box>
+            </Column>
           </Background>
         </Box>
       </Box>
